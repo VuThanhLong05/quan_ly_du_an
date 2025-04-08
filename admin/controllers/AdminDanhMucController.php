9029df740177aace3f8c9c2a3989eb2faa4f0566
THAO <thaovttph50036@gmail.com>
2025-04-08 15:42:33 +0700

    Đẩy 8/4

--- admin/controllers/AdminDanhMucController.php
+++ admin/controllers/AdminDanhMucController.php
@@ -15,4 +15,40 @@ class AdminDanhMucController
 
         require_once './views/danhmuc/listDanhMuc.php';
     }
+    public function formAddDanhMuc()
+    {
+        require_once './views/danhmuc/addDanhMuc.php';
+        // var_dump('Form thêm');
+    }
+
+    public function postAddDanhMuc()
+    {
+        // var_dump($_POST);
+
+        // Kiểm tra xem dữ liệu có submit lên không
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // code
+            // Lấy ra dữ liệu
+            $ten_danh_muc = $_POST['ten_danh_muc'];
+            $mo_ta = $_POST['mo_ta'];
+
+            // Tạo 1 mảng trống để chứa dữ liệu
+            $errors = [];
+            if (empty($ten_danh_muc)) {
+                $errors['ten_danh_muc'] = 'Tên danh mục không được để trống';
+            }
+
+            // Nếu không có lỗi tiến hành thêm danh mục
+            if (empty($errors)) {
+                // Nếu không có lỗi thì tiến hành thêm danh mục
+                // var_dump('ok');
+                $this->modelDanhMuc->insertDanhMuc($ten_danh_muc, $mo_ta);
+                header('location: ' . BASE_URL_ADMIN . '?act=danh-muc');
+                exit();
+            } else {
+                // Trả về form và lỗi
+                require_once './views/danhmuc/addDanhMuc.php';
+            }
+        }
+    }
 }
